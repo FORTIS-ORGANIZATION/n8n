25c98240cc6805a2c1da6a4174ff740f030caabd
Kavin Venkatesh <123236504+Kavin-Venkatesh@users.noreply.github.com>
2026-05-08 18:19:10 +0530

    Merged via WhatsApp approval

--- main.java
+++ main.java
@@ -1,6 +1,7 @@
 class Main{
   public static void main(String[] args){
     System.out.print("Hello world from main" );
+    System.out.print(sum(10, 12);
   }
 
   public static int sum(int num1  ,int num2){
