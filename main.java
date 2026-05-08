dab6a6c860ccf6c29f684990c5a0c58aa1bb7619
Kavin Venkatesh <123236504+Kavin-Venkatesh@users.noreply.github.com>
2026-05-08 18:14:01 +0530

    Add sum method to calculate the sum of two integers

--- main.java
+++ main.java
@@ -2,4 +2,7 @@ class Main{
   public static void main(String[] args){
     System.out.print("Hello world from main" );
   }
+
+  public static int sum(int num1  ,int num2){
+    return num1 + num2;
 }
